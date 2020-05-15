2eac301c48e8b022cb45a426bb77e780c14b69ea
CONWAY <kable2580@gmail.com>
2020-05-15 13:41:37 +0800

    format

--- app/src/main/java/com/brianlu/trashme/home/HomePresenter.java
+++ app/src/main/java/com/brianlu/trashme/home/HomePresenter.java
@@ -10,7 +10,6 @@ import com.brianlu.trashme.api.user.UserService;
 import com.brianlu.trashme.base.BasePresenter;
 import com.brianlu.trashme.model.LocationModel;
 import com.brianlu.trashme.model.MainPageModel;
-import com.brianlu.trashme.model.OperationType;
 import com.brianlu.trashme.model.OrderModel;
 import com.brianlu.trashme.model.StompMessageModel;
 import com.brianlu.trashme.model.TrashType;
@@ -21,24 +20,21 @@ import com.google.gson.JsonElement;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.util.Map;
-import java.util.Observable;
 
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 import ua.naiksoftware.stomp.dto.LifecycleEvent;
 
-
 class HomePresenter extends BasePresenter {
   private HomeView view;
 
-    @SuppressLint("CheckResult")
-    HomePresenter(HomeView view) {
-        this.view = view;
-        subscribeNoteRelay();
-        subscribeLocationRelay();
-        getHomePageData();
-
-    }
+  @SuppressLint("CheckResult")
+  HomePresenter(HomeView view) {
+    this.view = view;
+    subscribeNoteRelay();
+    subscribeLocationRelay();
+    getHomePageData();
+  }
 
   private void getHomePageData() {
     ConsumerService.getInstance()
@@ -69,11 +65,11 @@ class HomePresenter extends BasePresenter {
     String note = UserService.getInstance().noteRelay.getValue();
 
     LocationModel locationModel = UserService.getInstance().locationRelay.getValue();
-    if (locationModel == null ||
-        locationModel.getLatitude() == null ||
-        locationModel.getLongitude() == null ||
-        locationModel.getLocationName() == null ||
-        locationModel.getLocationName().isEmpty()) {
+    if (locationModel == null
+        || locationModel.getLatitude() == null
+        || locationModel.getLongitude() == null
+        || locationModel.getLocationName() == null
+        || locationModel.getLocationName().isEmpty()) {
       view.onSetMessage("請在下方點擊您的所在地", FancyToast.INFO);
       return;
     }
@@ -85,140 +81,135 @@ class HomePresenter extends BasePresenter {
     model.setLatitude(locationModel.getLatitude());
     model.setLongitude(locationModel.getLongitude());
     OrderService.getInstance().connect();
-    OrderService.getInstance().messageRelay.subscribe(new Observer<StompMessageModel>() {
-      @Override
-      public void onSubscribe(Disposable d) {
+    OrderService.getInstance()
+        .messageRelay
+        .subscribe(
+            new Observer<StompMessageModel>() {
+              @Override
+              public void onSubscribe(Disposable d) {}
 
-      }
+              @Override
+              public void onNext(StompMessageModel stompMessageModel) {
+                Log.i("HomePresenter", stompMessageModel.toString());
+                Map<String, Object> payload = stompMessageModel.getPayload();
+                switch (stompMessageModel.getOperationType()) {
+                  case SERVER_CREATED_ORDER:
+                    view.onSetOrderStatusView(View.VISIBLE);
+                    view.onSetOrderStateText("已建立訂單");
+                    return;
+                  case SERVER_FINISHED_ORDER:
+                    view.onSetOrderStatusView(View.GONE);
+                    view.onSetOrderStateText("");
+                    onComplete();
+                    return;
+                  case OTHER:
+                    return;
+                }
 
-      @Override
-      public void onNext(StompMessageModel stompMessageModel) {
-        Log.i("HomePresenter", stompMessageModel.toString());
-        Map<String, Object> payload = stompMessageModel.getPayload();
-        switch (stompMessageModel.getOperationType()) {
-          case SERVER_CREATED_ORDER:
-            view.onSetOrderStatusView(View.VISIBLE);
-            view.onSetOrderStateText("已建立訂單");
-            return;
-          case SERVER_FINISHED_ORDER:
-            view.onSetOrderStatusView(View.GONE);
-            view.onSetOrderStateText("");
-            onComplete();
-            return;
-          case OTHER:
-            return;
-        }
+                if (payload == null || payload.isEmpty()) {
+                  return;
+                }
+                WaiterInfoModel waiterInfoModel = mapToModel(payload, WaiterInfoModel.class);
+                String name = waiterInfoModel.getPickupUser();
+                int distance =
+                    (int)
+                        getDistanceOfMeter(
+                            locationModel.getLatitude(),
+                            waiterInfoModel.getLatitude(),
+                            locationModel.getLongitude(),
+                            waiterInfoModel.getLongitude());
+                switch (stompMessageModel.getOperationType()) {
+                  case SERVER_ACCEPTED_ORDER:
+                    view.onSetOrderStatusView(View.VISIBLE);
+                    view.onSetOrderStateText(name + " 距離" + distance / 1000 + "公尺 已接受訂單");
+                    return;
+                  case LOCATION_UPDATE:
+                    view.onSetOrderStatusView(View.VISIBLE);
+                    view.onSetOrderStateText(name + " 距離" + distance / 1000 + "公尺 正在移動...");
+                    return;
+                }
+              }
 
-        if(payload==null || payload.isEmpty()) { return;}
-        WaiterInfoModel waiterInfoModel = mapToModel(payload, WaiterInfoModel.class);
-        String name = waiterInfoModel.getPickupUser();
-        int distance = (int) getDistanceOfMeter(locationModel.getLatitude(),waiterInfoModel.getLatitude(), locationModel.getLongitude(), waiterInfoModel.getLongitude());
-        switch (stompMessageModel.getOperationType()) {
-          case SERVER_ACCEPTED_ORDER:
+              @Override
+              public void onError(Throwable e) {
+                Log.i("HomePresenter", e.getMessage());
+              }
 
-            view.onSetOrderStatusView(View.VISIBLE);
-            view.onSetOrderStateText(name + " 距離"+ distance/1000 +"公尺 已接受訂單");
-            return;
-          case LOCATION_UPDATE:
-            view.onSetOrderStatusView(View.VISIBLE);
-            view.onSetOrderStateText(name + " 距離"+ distance/1000 +"公尺 正在移動...");
-            return;
-        }
+              @Override
+              public void onComplete() {}
+            });
+    OrderService.getInstance()
+        .typeRelay
+        .subscribe(
+            new Observer<LifecycleEvent.Type>() {
+              @Override
+              public void onSubscribe(Disposable d) {}
 
+              @Override
+              public void onNext(LifecycleEvent.Type type) {
+                Log.i("HomePresenter", LifecycleEvent.Type.OPENED.toString());
+                if (type == LifecycleEvent.Type.OPENED) {
+                  OrderService.getInstance().createOrder(model);
+                }
+                onComplete();
+              }
 
-      }
+              @Override
+              public void onError(Throwable e) {}
 
-      @Override
-      public void onError(Throwable e) {
-        Log.i("HomePresenter", e.getMessage());
-      }
-
-      @Override
-      public void onComplete() {
-
-      }
-    });
-    OrderService.getInstance().typeRelay.subscribe(new Observer<LifecycleEvent.Type>() {
-      @Override
-      public void onSubscribe(Disposable d) {
-
-      }
-
-      @Override
-      public void onNext(LifecycleEvent.Type type) {
-        Log.i("HomePresenter", LifecycleEvent.Type.OPENED.toString());
-        if (type == LifecycleEvent.Type.OPENED){
-          OrderService.getInstance().createOrder(model);
-        }
-        onComplete();
-      }
-
-      @Override
-      public void onError(Throwable e) {
-
-      }
-
-      @Override
-      public void onComplete() {
-
-      }
-    });
+              @Override
+              public void onComplete() {}
+            });
   }
 
-    void logout() {
-        UserService.getInstance().saveUser(new User());
-        view.moveToLogin();
-    }
+  void logout() {
+    UserService.getInstance().saveUser(new User());
+    view.moveToLogin();
+  }
 
-    private void subscribeNoteRelay(){
-        UserService.getInstance().noteRelay.subscribe(new Observer<String>() {
-            @Override
-            public void onSubscribe(Disposable d) {
+  private void subscribeNoteRelay() {
+    UserService.getInstance()
+        .noteRelay
+        .subscribe(
+            new Observer<String>() {
+              @Override
+              public void onSubscribe(Disposable d) {}
 
-            }
-
-            @Override
-            public void onNext(String note) {
+              @Override
+              public void onNext(String note) {
                 view.onSetNote(note);
-            }
+              }
 
-            @Override
-            public void onError(Throwable e) {
+              @Override
+              public void onError(Throwable e) {}
 
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        });
-    }
-
-    private void subscribeLocationRelay(){
-    UserService.getInstance().locationRelay.subscribe(new Observer<LocationModel>() {
-      @Override
-      public void onSubscribe(Disposable d) {
-
-      }
-
-      @Override
-      public void onNext(LocationModel model) {
-        view.onSetLocation(model);
-      }
-
-      @Override
-      public void onError(Throwable e) {
-
-      }
-
-      @Override
-      public void onComplete() {
-
-      }
-    });
+              @Override
+              public void onComplete() {}
+            });
   }
 
-  private <T> T mapToModel(Map<String, Object>map, Class<T> tClass) {
+  private void subscribeLocationRelay() {
+    UserService.getInstance()
+        .locationRelay
+        .subscribe(
+            new Observer<LocationModel>() {
+              @Override
+              public void onSubscribe(Disposable d) {}
+
+              @Override
+              public void onNext(LocationModel model) {
+                view.onSetLocation(model);
+              }
+
+              @Override
+              public void onError(Throwable e) {}
+
+              @Override
+              public void onComplete() {}
+            });
+  }
+
+  private <T> T mapToModel(Map<String, Object> map, Class<T> tClass) {
     Gson gson = new Gson();
     JsonElement jsonElement = gson.toJsonTree(map);
     return gson.fromJson(jsonElement, tClass);
@@ -230,14 +221,18 @@ class HomePresenter extends BasePresenter {
     double radLat2 = rad(lat2);
     double a = radLat1 - radLat2;
     double b = rad(lng1) - rad(lng2);
-    double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2)
-        + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+    double s =
+        2
+            * Math.asin(
+                Math.sqrt(
+                    Math.pow(Math.sin(a / 2), 2)
+                        + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
     s = s * EARTH_RADIUS;
     s = Math.round(s * 10000) / 10000;
     return s;
   }
+
   private static double rad(double d) {
     return d * Math.PI / 180.0;
   }
-
 }
